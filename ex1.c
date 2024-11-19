16a0f41c35140f2ce68f1787a72ff9fadad36f37
Sagi1832 <186939561+Sagi1832@users.noreply.github.com>
2024-11-19 17:09:56 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,8 +1,12 @@
 /******************
-Name:sgai esformes
+Name: sagi esformes
 ID: 328416219
-Assignment:1
+Assignment: 1
 *******************/
+
+
+// REMIDER : YOU CANT USE ANY CONTROL FLOW OPERATIONS OR FUNCTIONS, ONLY BITWISE.
+
 #include<stdio.h>
 #include<stdlib.h>
 void task1();
@@ -28,9 +32,9 @@ void task1()
     int pos = 0;
     int bit = 0;
     printf("What bit:\n");
-    printf("Please enter a number: ");
+    printf("Please enter a number:\n");
     scanf("%d", &num);
-    printf("Please enter a position: ");
+    printf("Please enter a position:\n");
     scanf("%d", &pos);
     bit = (num >> pos) & 1;
     printf("The bit in position %d of number %d is: %d\n\n\n", pos, num, bit);
@@ -43,9 +47,10 @@ void task2()
 {
     int num = 0;
     int pos = 0;
-    printf("Please enter a number: ");
+    printf("Set bit:\n");
+    printf("Please enter a number:\n");
     scanf("%d", &num);
-    printf("Please enter a position: ");
+    printf("Please enter a position:\n");
     scanf("%d", &pos);
     int new_num_on = num | (1 << pos);
     int new_num_off = num & ~(1 << pos);
@@ -60,9 +65,10 @@ void task3()
 {
     int num = 0;
     int pos = 0;
-    printf("Please enter a number: ");
+    printf("Toggle bit:\n");
+    printf("Please enter a number:\n");
     scanf("%d", &num);
-    printf("Please enter a position: ");
+    printf("Please enter a position:\n");
     scanf("%d", &pos);
     int new_num = num ^ (1 << num);
     printf("Number with bit %d toggled: %d\n\n\n", pos, new_num);
@@ -74,7 +80,8 @@ void task3()
 void task4()
 {
     int num = 0;
-    printf("Please enter a number: ");
+    printf("Even - Odd:\n");
+    printf("Please enter a number:\n");
     scanf("%d", &num);
     int last_bit_1 = ~((num >> 0) & 1);
     int last_bit_2 = (last_bit_1 >> 0 & 1);
@@ -89,17 +96,19 @@ void task5()
     int num_1 = 0;
     int num_2 =0;
     int sum_of_nums = 0;
-    printf("Please enter the first number (octal): ");
+    printf("3, 5, 7, 11:\n");
+    printf("Please enter the first number (octal):\n");
     scanf("%o", &num_1);
-    printf("Please enter the second number (octal): ");
+    printf("Please enter the second number (octal):\n");
     scanf("%o", &num_2);
     sum_of_nums = num_1 + num_2;
-    printf("The sum in hexadecimal: %x\n", sum_of_nums);
+    printf("The sum in hexadecimal: %X\n", sum_of_nums);
     int bit_3 = (sum_of_nums >> 3) & 1;
     int bit_5 = (sum_of_nums >> 5) & 1;
     int bit_7 = (sum_of_nums >> 7) & 1;
     int bit_11 = (sum_of_nums >> 11) & 1;
     printf("The 3,5,7,11 bits are: %d%d%d%d\n\n\n", bit_3, bit_5, bit_7, bit_11);
+    printf("Bye!");
 
 
 
